5ed4e014c012c97d6e0a65c08c185bf8f99ab30d
falchon100 <ovnicrofordz@gmail.com>
2023-09-25 22:52:50 -0300

    Test desafio complementario

--- 41-Testing Avanzado/src/middleware/auth.js
+++ 41-Testing Avanzado/src/middleware/auth.js
@@ -4,7 +4,7 @@ export const requireLogin = (req, res, next) => {
     if(req.session.user){
         next()
     }else{
-        res.render('login', { status: 'Necesita Logearse, para ver los productos'})
+        res.status(401).render('login', { status: 'Necesita Logearse, para ver los productos'})
     }
 }
 //si no hay una session , puede continuar
